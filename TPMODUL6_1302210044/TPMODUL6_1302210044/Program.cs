a28d11e733164dbd57ba0943dd7873a436558ee1
shieldadchr <shielda.diasavitri@gmail.com>
2023-03-26 21:55:54 +0700

    SayaTubeVideo

--- TPMODUL6_1302210044/TPMODUL6_1302210044/Program.cs
+++ TPMODUL6_1302210044/TPMODUL6_1302210044/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        SayaTubeVideo sayaTubeVideo = new SayaTubeVideo("Tutorial Design by Contract [Shielda Diasavitri Chaerry]");
+        sayaTubeVideo.IncreasePlayCount(500);
+        sayaTubeVideo.PrintVideoDetails();
+        sayaTubeVideo.IncreasePlayCount(2147483647);
+    }
+}
